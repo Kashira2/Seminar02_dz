890eb4b16a85e8857640d84f56549fa37e5b5816
Kashira2 <nekit.egorov@icloud.com>
2022-08-23 00:30:13 +0300

    dz2

---
+++ Dz02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 100)
+{
+    Console.WriteLine("Третьего числа нет ");
+}
+else
+{    
+    while (N >= 1000)
+    {
+            N = N / 10;
+        
+    }
+    Console.WriteLine(N % 10);
+}
