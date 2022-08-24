d1de176a5a4a220b5f0abc1a1f3db3b2bc7628b2
Kashira2 <nekit.egorov@icloud.com>
2022-08-25 02:33:43 +0300

    programmers

---
+++ Dz04/Program.cs
@@ -0,0 +1,17 @@
+﻿void Programmers()
+{
+    int programmers = 1114;
+    //int programmers = new Random().Next(1, 1000);
+    Console.Write(programmers);
+    if ( programmers % 10 == 1) Console.Write(" программист");
+    else if ( programmers % 10 == 2) Console.Write(" программиста");
+    else if ( programmers % 10 == 3) Console.Write(" программиста");
+    else if ( programmers % 10 == 4) Console.Write(" программиста");
+    else if ( programmers % 100 == 11) Console.Write(" программистов");
+    else if ( programmers % 100 == 12) Console.Write(" программистов");
+    else if ( programmers % 100 == 13) Console.Write(" программистов");
+    else if ( programmers % 100 == 14) Console.Write(" программистов");
+    else Console.Write(" программистов");
+}
+
+Programmers();
